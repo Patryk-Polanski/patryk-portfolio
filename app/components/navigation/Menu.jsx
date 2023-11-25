315847cc10a34eecbcb7689b67225530a59286f2
Patryk-Polanski <pat.polanski@gmail.com>
2023-11-25 05:24:40 +0000

    Work on menu svg clip paths and animations (WIP)

--- app/components/navigation/Menu.jsx
+++ app/components/navigation/Menu.jsx
@@ -1,23 +1,80 @@
+'use client';
+
+import Image from 'next/image';
+import { useSearchParams } from 'next/navigation';
+
+import LinkHome from '../ui/svg/LinkHome';
+import LinkPortfolio from '../ui/svg/LinkPortfolio';
+import LinkTechStack from '../ui/svg/LinkTechStack';
+import LinkExperience from '../ui/svg/LinkExperience';
+import LinkAbout from '../ui/svg/LinkAbout';
+
 import styles from './Menu.module.css';
 
-const links = ['Home', 'Portfolio', 'Tech Stack', 'Experience', 'About'];
+const links = [
+  {
+    name: 'Home',
+    id: 'homePath',
+    elementActive: <LinkHome id='homePath' />,
+    element: <LinkHome />,
+  },
+  {
+    name: 'Portfolio',
+    id: 'portfolioPath',
+    elementActive: <LinkPortfolio id='portfolioPath' />,
+    element: <LinkPortfolio />,
+  },
+  {
+    name: 'Tech-Stack',
+    id: 'techStackPath',
+    elementActive: <LinkTechStack id='techStackPath' />,
+    element: <LinkTechStack />,
+  },
+  {
+    name: 'Experience',
+    id: 'experiencePath',
+    elementActive: <LinkExperience id='experiencePath' />,
+    element: <LinkExperience />,
+  },
+  {
+    name: 'About',
+    id: 'aboutPath',
+    elementActive: <LinkAbout id='aboutPath' />,
+    element: <LinkAbout />,
+  },
+];
 
 export default function Menu({ onLinkClick }) {
+  const searchParams = useSearchParams();
+
+  console.log(searchParams);
   return (
     <nav className={styles.menu}>
       <ul>
         {links.map((link) => (
-          <li key={link}>
+          <li key={link.name}>
             <a
               onClick={onLinkClick}
-              className={`d1 outlined-text ${
-                link.toLocaleLowerCase() === 'home'
-                  ? 'outlined-text--active'
-                  : ''
+              className={`${styles.menuLink} ${
+                link.name.toLocaleLowerCase() === 'portfolio' &&
+                styles.menuLinkActive
               }`}
-              href={`#${link.toLowerCase().replace(' ', '-')}`}
+              // className={`d1 outlined-text ${
+              //   link.toLocaleLowerCase() === 'home'
+              //     ? 'outlined-text--active'
+              //     : ''
+              // }`}
+              href={`#${link.name.toLowerCase().replace(' ', '-')}`}
             >
-              {link}
+              <span className='visually-hidden'>{link.name}</span>
+              {link.element}
+              {link.elementActive}
+              {link.name.toLocaleLowerCase() === 'portfolio' && (
+                <div
+                  className={styles.menuImage}
+                  style={{ clipPath: `url(#${link.id})` }}
+                />
+              )}
             </a>
           </li>
         ))}
