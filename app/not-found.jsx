5969e8260fd7a79c426a1047be23fcf462bb3738
patryk-polanski <pat.polanski@gmail.com>
2024-01-06 18:52:26 +0100

    add custom not-found page

---
+++ app/not-found.jsx
@@ -0,0 +1,13 @@
+import Button from './components/ui/Button';
+
+import styles from './NotFound.module.css';
+
+export default function NotFoundPage() {
+  return (
+    <div className={styles.notFound}>
+      <h2>404</h2>
+      <p>The page could not be found.</p>
+      <Button link='/' variation='primary' text='Home' />
+    </div>
+  );
+}
