21d5cc4412d8db496d77c7b692ac847f5220d656
patryk-polanski <pat.polanski@gmail.com>
2024-01-15 16:11:26 +0000

    hide footer for loading page

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,7 +2,7 @@ import styles from './Loading.module.css';
 
 export default function loadingPage() {
   return (
-    <div className={styles.loading}>
+    <div className={`loading ${styles.loading}`}>
       <div className={styles.spinner} />
     </div>
   );
