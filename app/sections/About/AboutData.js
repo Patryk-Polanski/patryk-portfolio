781d4fc0e4934cc97bf12cf7315d818a80d9ac72
patryk-polanski <pat.polanski@gmail.com>
2025-09-03 16:05:52 +0100

    update CV and experience section

--- app/sections/About/AboutData.js
+++ app/sections/About/AboutData.js
@@ -1,20 +1,20 @@
 export const AboutData = {
   about: [
-    'Welcome! My name is Patryk Polanski, a creative software engineer who writes clean, elegant and concise code. I occasionally dabble in UI/UX design and I do not shy away from diving into other cool visual media.',
-    'In recent years, I have worked for diverse companies, crafting a variety of websites ranging from WordPress news outlets and E-commerce Shopify sites to React/Next.js/Node.js applications.',
-    'What excites me the most is creating cutting-edge, visually attractive websites & apps that create lasting impressions on the users both in terms of pleasing designs as well as intuitive user experiences.',
-    'Currently on the lookout for my next JavaScript adventure, primarily focusing on React/Next.js or Node.js. While I am based in London, I’m open to packing my bags for promising opportunities elsewhere!',
+    "Welcome! My name is Patryk Polanski, a creative software engineer who writes clean, elegant and concise code. I occasionally dabble in UI/UX design and I do not shy away from diving into other cool visual media.",
+    "In recent years, I have worked for diverse companies, crafting a variety of websites ranging from WordPress news outlets and E-commerce Shopify sites to React/Next.js/Node.js applications.",
+    "What excites me the most is creating cutting-edge, visually attractive websites & apps that create lasting impressions on the users both in terms of pleasing designs as well as intuitive user experiences.",
+    "Currently on the lookout for my next JavaScript adventure, primarily focusing on React/React Native/Next.js or Node.js. While I am based in the EU, I’m open to packing my bags for promising opportunities elsewhere!",
   ],
   testimonials: [
     {
       testimonial:
-        'During his 12 month placement at Soapbox, Patryk became an invaluable member of the team making significant contributions to lots of our projects. A keen learner, he always showed an admirable willingness and aptitude in his work and by the end of the placement was producing a really high standard of work. His friendly and calm nature also made him a great fit within the team.',
-      author: 'Stuart, Soapbox',
+        "During his 12 month placement at Soapbox, Patryk became an invaluable member of the team making significant contributions to lots of our projects. A keen learner, he always showed an admirable willingness and aptitude in his work and by the end of the placement was producing a really high standard of work. His friendly and calm nature also made him a great fit within the team.",
+      author: "Stuart, Soapbox",
     },
     {
       testimonial:
-        'Patryk has done good work at kinetik - producing good quality work ranging from UI/UX and infographics to technical SEO. He is flexible with his approach, is a keen learner and is solution focused. He has very good communication skills and keeps all stakeholders informed. Patryk would be a very good addition to any design/technical team.',
-      author: 'Ketan, Kinetik',
+        "Patryk has done good work at kinetik - producing good quality work ranging from UI/UX and infographics to technical SEO. He is flexible with his approach, is a keen learner and is solution focused. He has very good communication skills and keeps all stakeholders informed. Patryk would be a very good addition to any design/technical team.",
+      author: "Ketan, Kinetik",
     },
   ],
 };
